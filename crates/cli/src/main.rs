95a9f8148f0586db03e723d1dcf4978c934a25aa
Gaurav Nardia <codegaurav07@gmail.com>
2026-09-14 20:59:18 +0530

    Feat: add fn to call redis connection

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    jobrail_redis::test_connection()
+        .await
+        .expect("Failed to connect to Redis");
 }
